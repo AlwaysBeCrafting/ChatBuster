ae752c33a4eda300219f5fb330d68f206f930818
Alexander Prime <rainesabaal@gmail.com>
2016-09-29 14:42:11 -0700

    Detect and record collisions

--- core/src/stream/alwaysbecrafting/chatbuster/ecs/component/BoundingBoxComponent.java
+++ core/src/stream/alwaysbecrafting/chatbuster/ecs/component/BoundingBoxComponent.java
@@ -1,22 +1,30 @@
 package stream.alwaysbecrafting.chatbuster.ecs.component;
 
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
 //==============================================================================
 public class BoundingBoxComponent {
 	//--------------------------------------------------------------------------
 
-	public int left, bottom, right, top;
+	public Rectangle rect;
 
 	//--------------------------------------------------------------------------
 
-	public BoundingBoxComponent() {}
+	public BoundingBoxComponent() {
+		rect = new Rectangle();
+	}
 
 	//--------------------------------------------------------------------------
 
 	public BoundingBoxComponent( int left, int bottom, int right, int top ) {
-		this.left = left;
-		this.bottom = bottom;
-		this.right = right;
-		this.top = top;
+		rect = new Rectangle( left, bottom, right - left, top - bottom );
+	}
+
+	//--------------------------------------------------------------------------
+
+	public boolean intersects( BoundingBoxComponent other ) {
+		return Intersector.intersectRectangles( rect, other.rect, null );
 	}
 
 	//--------------------------------------------------------------------------
