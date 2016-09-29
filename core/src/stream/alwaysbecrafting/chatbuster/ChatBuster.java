0cefa47de4302e38741e3fe5a42bafb575804f23
Alexander Prime <rainesabaal@gmail.com>
2016-09-29 16:44:39 -0700

    Hook up pieces for state change on jump/shoot

--- core/src/stream/alwaysbecrafting/chatbuster/ChatBuster.java
+++ core/src/stream/alwaysbecrafting/chatbuster/ChatBuster.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import stream.alwaysbecrafting.chatbuster.ecs.Entities;
 import stream.alwaysbecrafting.chatbuster.ecs.system.BackgroundRenderSystem;
 import stream.alwaysbecrafting.chatbuster.ecs.system.BoxRenderSystem;
-import stream.alwaysbecrafting.chatbuster.ecs.system.ChatControlSystem;
 import stream.alwaysbecrafting.chatbuster.ecs.system.PlayerInputSystem;
 import stream.alwaysbecrafting.chatbuster.ecs.system.SpriteRenderSystem;
 import stream.alwaysbecrafting.flare.GameEngine;
@@ -29,12 +28,13 @@ public class ChatBuster extends ApplicationAdapter {
 	@Override public void create() {
 		engine = new GameEngine();
 
-		engine.add( new ChatControlSystem( "alwaysbecrafting", TOKEN ));
+//		engine.add( new ChatControlSystem( "alwaysbecrafting", TOKEN ));
 		engine.add( new BackgroundRenderSystem() );
 		engine.add( new SpriteRenderSystem() );
 		engine.add( new PlayerInputSystem() );
 		engine.add( new BoxRenderSystem() );
 
+		engine.add( Entities.makePlayerCharacter() );
 		engine.add( Entities.makeWall( 0, 0, 320, 40 ));
 	}
 
--- core/src/stream/alwaysbecrafting/chatbuster/ecs/component/AllyStateComponent.java
+++ core/src/stream/alwaysbecrafting/chatbuster/ecs/component/AllyStateComponent.java
@@ -10,37 +10,79 @@ public class AllyStateComponent {
 	public StateMachine characterState;
 	public StateMachine gunState;
 
+	//--------------------------------------------------------------------------
+
 	public AllyStateComponent() {
 		characterState = new StateMachine();
 		gunState = new StateMachine();
 
 
-		characterState.add( "zorpIn", new State() );
+		characterState.add( "zorpIn",     new State()     );
+		characterState.add( "stand",      new State()     );
+		characterState.add( "jump",       new JumpState() );
+		characterState.add( "fall",       new FallState() );
+		characterState.add( "takeDamage", new State()     );
+		characterState.add( "zorpOut",    new State()     );
 
-		characterState.add( "stand", new State() );
-
-		characterState.add( "jump", new State() );
-
-		characterState.add( "fall", new State() );
-
-		characterState.add( "takeDamage", new State() );
-
-		characterState.add( "zorpOut", new State() );
-
-
-		gunState.add( "idle", new State() );
-
-		gunState.add( "charge", new State() {
-			private int chargeLevel;
-		} );
-
-		gunState.add( "shoot", new State() );
+		gunState.add( "idle",   new State()       );
+		gunState.add( "charge", new ChargeState() );
+		gunState.add( "shoot",  new ShootState()  );
 
 
 		characterState.change( "fall" );
 		gunState.change( "idle" );
 	}
 
+	//--------------------------------------------------------------------------
+
+	//==========================================================================
+	private class JumpState extends State {
+		//----------------------------------------------------------------------
+
+		VelocityComponent velocityComp;
+
+		//----------------------------------------------------------------------
+
+		@Override public void onEnter( Object... params ) {
+			velocityComp = (VelocityComponent)params[0];
+			velocityComp.v = 5;
+		}
+
+		//----------------------------------------------------------------------
+
+		@Override public void onUpdate( double deltaTime ) {
+			if ( velocityComp.v < 0 ) characterState.change( "fall", velocityComp );
+		}
+
+		//----------------------------------------------------------------------
+	}
+	//--------------------------------------------------------------------------
+
+	//==========================================================================
+	private class FallState extends State {}
+	//--------------------------------------------------------------------------
+
+
+	//==========================================================================
+	private class ChargeState extends State {
+		//----------------------------------------------------------------------
+
+		private int chargeLevel;
+
+		//----------------------------------------------------------------------
+	}
+	//--------------------------------------------------------------------------
+
+	//==========================================================================
+	private class ShootState extends State {
+		//----------------------------------------------------------------------
+
+		@Override public void onEnter( Object... params ) {
+			Integer strength = (Integer)params[0];
+		}
+
+		//----------------------------------------------------------------------
+	}
 	//--------------------------------------------------------------------------
 }
 //------------------------------------------------------------------------------
