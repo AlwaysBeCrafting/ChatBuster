6d01faf1936d6714ad2f3cc2848e37665dae3e69
Alexander Prime <rainesabaal@gmail.com>
2016-10-06 16:58:53 -0700

    Use SpriteMap in SpriteComponent

--- core/src/stream/alwaysbecrafting/chatbuster/ecs/component/render/SpriteComponent.java
+++ core/src/stream/alwaysbecrafting/chatbuster/ecs/component/render/SpriteComponent.java
@@ -4,71 +4,37 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
+import stream.alwaysbecrafting.chatbuster.data.SpriteMap;
+
 //==============================================================================
 public class SpriteComponent {
 	//--------------------------------------------------------------------------
 
 	public Sprite sprite;
-	public int cellWidth;
-	public int cellHeight;
 
 	public Vector2 origin = new Vector2();
 
-
-	private int cellX, cellY;
+	private final SpriteMap SPRITE_MAP;
 
 	//--------------------------------------------------------------------------
 
 	public SpriteComponent( String path ) {
 		sprite = new Sprite( new Texture( path ));
-		cellWidth = sprite.getTexture().getWidth();
-		cellHeight = sprite.getTexture().getHeight();
+		SPRITE_MAP = new SpriteMap( (int)sprite.getWidth(), (int)sprite.getHeight() );
 	}
 
 	//--------------------------------------------------------------------------
 
-	public SpriteComponent( String path, int cellWidth, int cellHeight ) {
-		this( path );
-
-		this.cellWidth = cellWidth;
-		this.cellHeight = cellHeight;
-
-		setCell( 0, 0 );
+	public SpriteComponent( String path, SpriteMap spriteMap ) {
+		sprite = new Sprite( new Texture( path ));
+		SPRITE_MAP = spriteMap;
 	}
 
 	//--------------------------------------------------------------------------
 
-	public SpriteComponent( String path, int cellWidth, int cellHeight, int originX, int originY ) {
-		this( path, cellWidth, cellHeight );
-
+	public SpriteComponent( String path, SpriteMap spriteMap, int originX, int originY ) {
+		this( path, spriteMap );
 		origin.set( originX, originY );
-	}
-
-	//--------------------------------------------------------------------------
-
-	public void setCell( int cellX, int cellY ) {
-		this.cellX = cellX;
-		this.cellY = cellY;
-
-		sprite.setRegion(
-				cellX * cellWidth,
-				cellY * cellHeight,
-				cellWidth,
-				cellHeight );
-	}
-
-	//--------------------------------------------------------------------------
-
-	public void setCellX( int cellX ) {
-		this.cellX = cellX;
-		setCell( cellX, cellY );
-	}
-
-	//--------------------------------------------------------------------------
-
-	public void setCellY( int cellY ) {
-		this.cellY = cellY;
-		setCell( cellX, cellY );
 	}
 
 	//--------------------------------------------------------------------------
